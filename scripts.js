953ee0c91474a8d989292fe53639358dd67ad52b
svvdevell <svvdevel@gmail.com>
2022-08-17 15:58:46 +0300

    added 2 task

--- scripts.js
+++ scripts.js
@@ -4,3 +4,18 @@
 let userName = prompt('Как Вас зовут?');
 alert(`Вас зовут ${userName}!`); 
 console.log( `Имя пользователя: ${userName}`);
+
+// 2
+
+let a = 123;
+let b = 10;
+let result = a + b;
+alert(`Результат сложения: ${result}`);
+let result2 = a - b;
+alert(`Результат вычитания: ${result2}`);
+let result3 = a / b;
+alert(`Результат деления: ${result3}`);
+let result4 = a * b;
+alert(`Результат умножения: ${result4}`);
+
+console.log(result,result2,result3,result4);
